5c1f1fc33051e57f8f45ff0b08286e921b52735b
Dan <danielandrewvoss@gmail.com>
2016-09-15 09:53:31 -0400

    Get html with URL object

---
+++ src/com/dvoss/Webshed.java
@@ -0,0 +1,29 @@
+package com.dvoss;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Dan on 9/15/16.
+ */
+
+public class Webshed {
+
+    public static void main(String[] args) throws IOException {
+
+        // uses InputStreamReader to read html with URL object
+        // from Niemeyer and Leuck
+
+        URL u = new URL("https://danvoss.github.io/index.html");
+        BufferedReader b = new BufferedReader(new InputStreamReader(u.openStream()));
+
+        String l;
+        while ((l = b.readLine()) != null) {
+            System.out.println(l);
+        }
+        b.close();
+    }
+}
