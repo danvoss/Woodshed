08f3cb20ed19cf36b1a8f854059f818c4c927d4e
Dan <danielandrewvoss@gmail.com>
2016-09-27 12:04:04 -0400

    Add new instrument

--- src/com/dvoss/Midi.java
+++ src/com/dvoss/Midi.java
@@ -12,8 +12,27 @@ public class Midi {
         Synthesizer synth = MidiSystem.getSynthesizer();
         synth.open();
 
+        // simple piano middle C
+//        MidiChannel[] mc = synth.getChannels();
+//        mc[1].noteOn(60, 200);
+//        Thread.sleep(2000);
+//        mc[1].noteOff(60);
+
+        Soundbank sb = synth.getDefaultSoundbank();
+        synth.loadAllInstruments(sb);
+        Instrument[] inst = synth.getLoadedInstruments();
+
+        // show list of all instruments:
+//        for (int i=0; i<inst.length; i++ ) {
+//            System.out.println(inst[i].getName());
+//        }
+
+        //set a new instrument (eg, electric piano):
+        Patch p = inst[5].getPatch();
+
         MidiChannel[] mc = synth.getChannels();
-        mc[1].noteOn(60, 100);
+        mc[1].programChange(p.getBank(), p.getProgram());
+        mc[1].noteOn(60, 200);
         Thread.sleep(2000);
         mc[1].noteOff(60);
 
