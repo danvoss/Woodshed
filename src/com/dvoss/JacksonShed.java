1ec259d7f4647f0295257dc6970addc1cafa54d6
Dan <danielandrewvoss@gmail.com>
2016-11-11 11:29:01 -0500

    Read and write JSON with Jackson

--- src/com/dvoss/JacksonShed.java
+++ src/com/dvoss/JacksonShed.java
@@ -10,12 +10,20 @@ import java.io.IOException;
 public class JacksonShed {
 
     public static void main(String[] args) throws IOException {
-
+        // read json
         ObjectMapper objectMapper = new ObjectMapper();
         String personJson = "{ \"name\" : \"Alice\", \"age\" : 25 }";
 
         Person p1 = objectMapper.readValue(personJson, Person.class);
         System.out.println("Name: " + p1.getName());
         System.out.println("Age: " + p1.getAge());
+
+        // write json
+        Person p2 = new Person();
+        p2.setName("Bob");
+        p2.setAge(40);
+
+        String json = objectMapper.writeValueAsString(p2);
+        System.out.println(json);
     }
 }
